1f0e1c9e94d4a6642c1bc06f8035f43e9db6db69
nickyonge <nick@duckpond.studio>
2025-07-28 23:35:36 -0700

    ui class prep, demo label

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,17 @@
 import(/* webpackPreload: true */ './../css/fonts.css');
 import(/* webpackPreload: true */ './../css/style.css');
 
+import { BuildUI } from './ui';
+
 window.addEventListener('load', function () {
     console.log("page loaded");
-    
-    var label = this.document.createElement("Label");
-    label.setAttribute("style", "font-weight:normal");
-    label.innerHTML = "Hello world? 124-08120598012958019825 Oops! H IiLl1!";
-    document.body.appendChild(label);
-    
+    DemoLabel();
+    BuildUI();
 });
+
+function DemoLabel() {
+    console.log("Creating demo label");
+    var label = document.createElement("Label");
+    label.innerHTML = "Hello world! 0123456789";
+    document.body.appendChild(label);
+}
---
+++ src/js/ui.js
@@ -0,0 +1,11 @@
+
+
+let topBar = null;
+let content = null;
+let artWindow = null;
+let dataWindow = null;
+let btmBar = null;
+
+export function BuildUI() {
+    
+}
