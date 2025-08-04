f58e91e739439befcc42fc8581e2dd91020aba9b
nickyonge <nick@duckpond.studio>
2025-08-04 12:27:08 -0700

    prep data window, tabs section

---
+++ src/js/uiData.js
@@ -0,0 +1,15 @@
+/* UI elements for the Data window - tabs, selection buttons, etc */
+
+/**
+ * Create the data window (tabs, options, info)
+ * @param {Element} dataWindow 
+ */
+export function CreateDataWindow(dataWindow) {
+
+    CreateTabs(dataWindow);
+
+}
+
+function CreateTabs(dataWindow) {
+
+}
--- src/js/uiMain.js
+++ src/js/uiMain.js
@@ -1,6 +1,7 @@
 /* Main UI generation */
 
 import { CreateHeaderFooter } from "./uiHdFt";
+import { CreateDataWindow } from "./uiData";
 import * as ui from './ui';
 
 const _useDemoBGColors = false;
@@ -8,9 +9,13 @@ const _useDemoBGColors = false;
 /** container div for all content on the page @type Element */
 let content = null;
 
+/** Header (top bar) element @type Element */
 let topBar = null;
+/** Art window, where the canvas rendering goes @type Element */
 let artWindow = null;
+/** Data window, where the tabs and info window goes @type Element */
 let dataWindow = null;
+/** Footer (bottom bar) element @type Element */
 let btmBar = null;
 
 export function BuildUI() {
@@ -24,6 +29,7 @@ export function BuildUI() {
     btmBar = ui.AddElementTo(content, 'footer');
 
     CreateHeaderFooter(topBar, btmBar);
+    CreateDataWindow(dataWindow);
 
     document.body.appendChild(content);
 
