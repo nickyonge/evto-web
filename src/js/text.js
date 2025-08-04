dd26cacd4423ad67483d3a5dcb235e1596e93d4a
nickyonge <nick@duckpond.studio>
2025-08-04 13:29:14 -0700

    tabs text and num count

--- src/js/text.js
+++ src/js/text.js
@@ -2,3 +2,7 @@
 
 export const EMAIL_PLACEHOLDER = "Duck Pond Newsletter";
 export const SLIDING_BUTTON = "Live now on Kickstarter!";
+
+// total tabs count: intro, size, features, colour/pattern, save/load
+export const TABS = ["Intro", "Size", "Features", "Colour & Pattern", "Save & Load"];
+export const TABS_NUM = TABS.length;
