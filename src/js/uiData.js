7b2f57c65d4acae63277024727c6d4bea5fe7db8
nickyonge <nick@duckpond.studio>
2025-08-06 23:44:41 -0700

    basic icons

--- src/js/uiData.js
+++ src/js/uiData.js
@@ -3,6 +3,13 @@
 import * as ui from "./ui";
 import * as txt from './text';
 
+import iconArt from '../assets/svg/icon-art.svg';
+import iconFeatures from '../assets/svg/icon-features.svg';
+import iconHome from '../assets/svg/icon-home.svg';
+import iconSave from '../assets/svg/icon-save.svg';
+import iconScale from '../assets/svg/icon-scale.svg';
+
+let iconArray = [iconHome, iconScale, iconFeatures, iconArt, iconSave];
 
 /**
  * Create the data window (tabs, options, info)
@@ -22,10 +29,18 @@ function CreateTabs(dataWindow) {
             ['radio', 'tab' + (i + 1), 'tab']);
         let tabLabel = ui.CreateElement('label'); // label element
         ui.AddElementAttribute(tabLabel, 'for', 'tab' + (i + 1));
+        // tab text and icon
         let tabText = ui.CreateDivWithClass('text');
         tabText.innerText = txt.TABS[i];
+        // let tabIcon = ui.CreateDivWithClass('icon');
+        // let img = ui.CreateImage(iconArray[i]);
+        // let tabIcon = ui.CreateDivWithClass('icon');
+        let tabIcon = ui.CreateImage(iconArray[i]);
+        ui.AddClassToDOMs('icon', tabIcon);
+        // tabIcon.appendChild(img);
         tabLabel.appendChild(tabText);
-        // add elements
+        tabLabel.appendChild(tabIcon);
+        // add tab elements to tabs list
         tabs.appendChild(tabInput);
         tabs.appendChild(tabLabel);
     }
