5c20c198b14bdec76503b44c12772f42baee1235
nickyonge <nick@duckpond.studio>
2025-08-11 13:58:00 -0700

    added CSS style getter to lilutils

--- src/js/lilutils.js
+++ src/js/lilutils.js
@@ -3,6 +3,25 @@
 /**
  * check if a string is null, empty, or whitespace
  * @param {string} str input string to test 
- * @returns true if blank, false if contains content
+ * @returns {boolean} true if blank, false if contains content
  */
 export const isBlank = str => !str || !str.trim();
+
+/** 
+ * Gets the CSS stylesheet for the page 
+ * @type {CSSStyleDeclaration}
+ */
+export const style = {
+    get value() {
+        if (!_style) {
+            if (!document.body) {
+                throw new Error("Body isn't yet loaded, don't call style until after window is loaded");
+            }
+            _style = window.getComputedStyle(document.body);
+        }
+        return _style;
+    }
+};
+/** local style reference for utils
+ * @type {CSSStyleDeclaration} */
+let _style;
