33b645847fd3414cb20c84370fc2b29a3aae2aff
nickyonge <nick@duckpond.studio>
2025-08-20 17:42:21 -0700

    3js setup and box CSS

--- src/js/contentArt.js
+++ src/js/contentArt.js
@@ -5,33 +5,67 @@ import * as ui from './ui';
 import { canvasInner } from './uiArt';
 
 export function SetupArtWindow() {
-    CreateDemo3DScene();
+    Create3DScene();
 }
 
-function CreateDemo3DScene() {
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    const renderer = new THREE.WebGLRenderer({
-        alpha: true,
-        antialias: false,
-        premultipliedAlpha: false
-    });
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    const cube = new THREE.Mesh(geometry, material);
-    
-    // renderer.setSize(window.innerWidth, window.innerHeight, false);
-    renderer.setSize(canvasInner.offsetWidth * 1, canvasInner.offsetHeight * 1, false);
-    
-    scene.add(cube);
-    camera.position.z = 5;
-    renderer.setAnimationLoop(Animate);
-    function Animate() {
-        cube.rotation.x += 0.01;
-        cube.rotation.y += 0.015;
-        renderer.render(scene, camera);
+let scene;
+let camera;
+let renderer;
+let canvas;
+let demoCube;
+
+const upscaleFactor = 2;
+
+const cameraNearClip = 1;
+const cameraFarClip = 12;
+const useOrthographicCamera = false;
+const camOrthoDivFactor = 128;
+const camPerspectiveFOV = 25;
+
+/** enables 3JS renderer antialiasing and premultiplied alpha. Expensive. @type {Boolean} */
+const hqVisuals = true;
+
+const useDemoCube = true;
+
+function Create3DScene() {
+    scene = new THREE.Scene();
+    if (useOrthographicCamera) {
+        camera = new THREE.OrthographicCamera(sceneWidth() / - camOrthoDivFactor, sceneWidth() / camOrthoDivFactor, sceneHeight() / camOrthoDivFactor, sceneHeight() / - camOrthoDivFactor, cameraNearClip, cameraFarClip);
+    } else {
+        camera = new THREE.PerspectiveCamera(camPerspectiveFOV, sceneAspect(), cameraNearClip, cameraFarClip);
     }
-    // renderer.domElement.id = 'threeJS';
-    ui.AddClassToDOMs('threeJS', renderer.domElement);
-    canvasInner.appendChild(renderer.domElement);
+    renderer = new THREE.WebGLRenderer({
+        alpha: true,
+        antialias: hqVisuals,
+        premultipliedAlpha: hqVisuals
+    });
+    canvas = renderer.domElement;
+    if (useDemoCube) {
+        const demoCubeGeo = new THREE.BoxGeometry(1, 1, 1);
+        const demoCubeMat = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+        demoCube = new THREE.Mesh(demoCubeGeo, demoCubeMat);
+        scene.add(demoCube);
+    }
+
+    renderer.setSize(sceneWidth() * upscaleFactor, sceneHeight() * upscaleFactor, false);
+
+    camera.position.z = 6.9;
+    renderer.setAnimationLoop(ThreeJSFrame);
+    ui.AddClassToDOMs('threeJS', canvas);
+    canvasInner.appendChild(canvas);
 }
+
+export function ThreeJSFrame() {
+    
+    if (useDemoCube) {
+        demoCube.rotation.x += 0.01;
+        demoCube.rotation.y += 0.015;
+    }
+
+    renderer.render(scene, camera);
+}
+
+
+function sceneWidth() { return canvasInner.offsetWidth; }
+function sceneHeight() { return canvasInner.offsetHeight; }
+function sceneAspect() { sceneHeight(); return !sceneHeight() ? 0 : sceneWidth() / sceneHeight(); }
