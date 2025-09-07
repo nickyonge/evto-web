d516aa144e2c373f8106d437386660d73a58da1d
nickyonge <nick@duckpond.studio>
2025-09-07 16:53:22 -0700

    wip dropdown w/ SVG

--- src/js/components/dropdown.js
+++ src/js/components/dropdown.js
@@ -4,6 +4,7 @@ export class DropdownList extends TitledComponent {
 
     #dropdown;
     #selected;
+    #svg;
     #optionsContainer;
     #options;
 
@@ -17,9 +18,21 @@ export class DropdownList extends TitledComponent {
         this.#selected = ui.CreateDivWithClass('ddSelected');
         ui.AddElementAttribute(this.#selected, 'data-label', '');
 
+        this.#svg = ui.CreateSVG(
+            [[arrowSVGPath, '#ffffff']],
+            [['height', '1em'], ['viewBox', '0 0 512 512']],
+            'arrow');
+
         // add elements 
         this.div.appendChild(this._titleElement);
         this.div.appendChild(this.#dropdown);
+        this.#dropdown.appendChild(this.#selected);
+        console.log("sel: " + this.#svg);
+
+        this.#selected.appendChild(this.#svg);
 
     }
 }
+
+/** path `d` attribute for SVG arrow */
+const arrowSVGPath = 'M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z';
