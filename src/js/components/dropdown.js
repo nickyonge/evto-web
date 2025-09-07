cae2524b51f6211eb109833cacbc92872019f8d5
nickyonge <nick@duckpond.studio>
2025-09-07 15:55:54 -0700

    wip build dropdown list HTML/JS

--- src/js/components/dropdown.js
+++ src/js/components/dropdown.js
@@ -2,15 +2,24 @@ import * as ui from "../ui";
 import { TitledComponent } from "./base";
 export class DropdownList extends TitledComponent {
 
+    #dropdown;
+    #selected;
+    #optionsContainer;
+    #options;
 
-
-    constructor(componentTitle) {
+    constructor(componentTitle, onSelectCallback, options, icons) {
         super(componentTitle);
 
         this.div = ui.CreateDivWithClass('dropdownContainer');
         ui.AddClassesToDOM(this.div, 'dropdownContainer');
         this._titleElement = ui.CreateDivWithClass('componentTitle', 'listTitle');
+        this.#dropdown = ui.CreateDivWithClass('dropdown');
+        this.#selected = ui.CreateDivWithClass('ddSelected');
+        ui.AddElementAttribute(this.#selected, 'data-label', '');
 
+        // add elements 
+        this.div.appendChild(this._titleElement);
+        this.div.appendChild(this.#dropdown);
 
     }
 }
