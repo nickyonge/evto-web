9966e089f13e708d008e9d712e59689c79d684c0
nickyonge <nick@duckpond.studio>
2025-09-07 16:53:16 -0700

    svg bugfixes

--- src/js/ui.js
+++ src/js/ui.js
@@ -318,19 +318,22 @@ export function CreateSVG(paths, attributes, ...cssClasses) {
         }
     }
     // add classes
-    AddClassesToDOM(div, ...cssClasses);
+    AddClassesToDOM(svg, ...cssClasses);
+    return svg;
 
-    // <svg 
-    //     xmlns="http://www.w3.org/2000/svg"
-    //     height="1em"
-    //     viewBox="0 0 512 512"
-    //     class="arrow"
-    // >
-    //     <path
-    //         fill="#ffffff"
-    //         d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z"
-    //     ></path>
-    // </svg>
+    /* EXAMPLE SVG: 
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        height="1em"
+        viewBox="0 0 512 512"
+        class="arrow"
+    >
+        <path
+            fill="#ffffff"
+            d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z"
+        ></path>
+    </svg>
+    */
 }
 /** Returns the SVG path WITHOUT `d="`...`"` or `d='`...`'` at the beginning/end
  * @param {string} path SVG path value 
