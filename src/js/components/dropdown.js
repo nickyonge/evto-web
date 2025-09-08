e3f3d418e92be1c8b0c645561cdb40cd93a7dac2
nickyonge <nick@duckpond.studio>
2025-09-07 18:23:38 -0700

    options HTML generation

--- src/js/components/dropdown.js
+++ src/js/components/dropdown.js
@@ -1,12 +1,16 @@
 import * as ui from "../ui";
 import { TitledComponent } from "./base";
+const initialValue = 0;
+
 export class DropdownList extends TitledComponent {
 
     #dropdown;
     #selected;
     #svg;
     #optionsContainer;
-    #options;
+    #optionsDivs;
+    #optionsInputs;
+    #optionsLabels;
 
     constructor(componentTitle, onSelectCallback, options, icons) {
         super(componentTitle);
@@ -16,21 +20,48 @@ export class DropdownList extends TitledComponent {
         this._titleElement = ui.CreateDivWithClass('componentTitle', 'listTitle');
         this.#dropdown = ui.CreateDivWithClass('dropdown');
         this.#selected = ui.CreateDivWithClass('ddSelected');
-        ui.AddElementAttribute(this.#selected, 'data-label', '');
-
+        if (options && options.length >= initialValue + 1) {
+            ui.AddElementAttribute(this.#selected, 'data-label', options[initialValue]);
+        } else {
+            ui.AddElementAttribute(this.#selected, 'data-label', '');
+        }
+        // create dropdown arrow SVG
         this.#svg = ui.CreateSVG(
             [[arrowSVGPath, '#ffffff']],
             [['height', '1em'], ['viewBox', '0 0 512 512']],
-            'arrow');
-
+            'ddArrow');
+        // create menu options 
+        this.#optionsDivs = [];
+        this.#optionsInputs = [];
+        this.#optionsLabels = [];
+        this.#optionsContainer = ui.CreateDivWithClass('ddOptions');
+        // iterate thru options 
+        for (let i = 0; i < options.length; i++) {
+            // create elements
+            let oDiv = ui.CreateDiv();
+            let oInput = ui.CreateInputWithID('radio', options[i]);
+            if (i == initialValue) {
+                ui.AddElementAttributes(oInput, ['name', 'checked'], ['ddOption', '']);
+            } else {
+                ui.AddElementAttribute(oInput, 'name', 'ddOption');
+            }
+            let oLabel = ui.CreateElementWithClass('label', 'ddOption');
+            ui.AddElementAttributes(oLabel, ['for', 'data-txt'], [options[i], options[i]]);
+            // push to arrays
+            this.#optionsDivs.push(oDiv);
+            this.#optionsInputs.push(oInput);
+            this.#optionsLabels.push(oLabel);
+            // add children to parents 
+            oDiv.appendChild(oInput);
+            oDiv.appendChild(oLabel);
+            this.#optionsContainer.appendChild(oDiv);
+        }
         // add elements 
         this.div.appendChild(this._titleElement);
         this.div.appendChild(this.#dropdown);
         this.#dropdown.appendChild(this.#selected);
-        console.log("sel: " + this.#svg);
-
         this.#selected.appendChild(this.#svg);
-
+        this.#dropdown.appendChild(this.#optionsContainer);
     }
 }
 
