df0757860bd313e0f410df198b3e1102d468fadd
nickyonge <nick@duckpond.studio>
2025-09-15 21:10:02 -0700

    wip costs for features, sm/md/lg costs todo

--- src/js/costs.js
+++ src/js/costs.js
@@ -11,4 +11,13 @@ export const SIZE_LG = 4;
 export const SIZE_LGP = 5;
 
 // ============= FEATURES =============================
+
+//TODO: make these accommodate cost differences in sm/md/lg 
+export const FEAT_LANDDETAIL = [0, 1, 3];
+export const FEAT_GCSLINES = [];
+export const FEAT_LABELLING = [];
+export const FEAT_TITLEBOX = [];
+export const FEAT_LANDLINES = [];
+export const FEAT_LANGUAGE = [];
+
 // ============= COLOUR & PATTERN ===================== 
