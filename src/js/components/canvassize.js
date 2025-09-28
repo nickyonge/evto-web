41627f412bb21a6681ff43626027839f63d16c83
nickyonge <nick@duckpond.studio>
2025-09-28 15:48:40 -0700

    canvassize css start

--- src/js/components/canvassize.js
+++ src/js/components/canvassize.js
@@ -4,8 +4,15 @@ import * as txt from '../text';
 
 export class CanvasSize extends TitledComponent {
 
+    #imageContainer;
+
     constructor(componentTitle) {
         super(componentTitle);
+
+        ui.AddClassToDOMs('canvasSize', this.div);
+        this.#imageContainer = ui.CreateDivWithClass('canvasSize', 'imageContainer');
+        this.div.appendChild(this.#imageContainer);
+
     }
 
 }
