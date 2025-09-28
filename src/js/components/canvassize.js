7460d964312b4f8e39265ee97d9f6f27397da8c1
nickyonge <nick@duckpond.studio>
2025-09-27 18:41:06 -0700

    basic canvas size component

---
+++ src/js/components/canvassize.js
@@ -0,0 +1,11 @@
+import * as ui from "../ui";
+import { TitledComponent } from "./base";
+import * as txt from '../text';
+
+export class CanvasSize extends TitledComponent {
+
+    constructor(componentTitle) {
+        super(componentTitle);
+    }
+
+}
