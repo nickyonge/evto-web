c4629710272d399e798f284f82db447e92ef2fe3
nickyonge <nick@duckpond.studio>
2025-09-28 16:25:07 -0700

    iteratively display all canvases

--- src/js/components/canvassize.js
+++ src/js/components/canvassize.js
@@ -2,9 +2,26 @@ import * as ui from "../ui";
 import { TitledComponent } from "./base";
 import * as txt from '../text';
 
+import canvasBase from '../../assets/png/canvases/evto_paintings_plain_faded.png';
+// import canvasBase from '../../assets/png/canvases/evto_paintings_plain.png';
+
+import canvasLgP from '../../assets/png/canvases/alpha_assets/cutouts_colour_frames/evto_canvas_frame_6lgp.png';
+import canvasLg from '../../assets/png/canvases/alpha_assets/cutouts_colour_frames/evto_canvas_frame_5lg.png';
+import canvasMdP from '../../assets/png/canvases/alpha_assets/cutouts_colour_frames/evto_canvas_frame_4mdp.png';
+import canvasMd from '../../assets/png/canvases/alpha_assets/cutouts_colour_frames/evto_canvas_frame_3md.png';
+import canvasSmP from '../../assets/png/canvases/alpha_assets/cutouts_colour_frames/evto_canvas_frame_2smp.png';
+import canvasSm from '../../assets/png/canvases/alpha_assets/cutouts_colour_frames/evto_canvas_frame_1sm.png';
+
+let canvasFramesArray = [canvasBase, canvasLgP, canvasLg, canvasMdP, canvasMd, canvasSmP, canvasSm];
+
 export class CanvasSize extends TitledComponent {
 
+    /** container div for all the images @type {HTMLElement} */
     #imageContainer;
+    /** base image of the canvas, underneath the individual frames @type {HTMLElement} */
+    #imgBase;
+    /** array of all frame images of the canvas @type {HTMLElement[]} */
+    #images;
 
     constructor(componentTitle) {
         super(componentTitle);
@@ -13,6 +30,71 @@ export class CanvasSize extends TitledComponent {
         this.#imageContainer = ui.CreateDivWithClass('canvasSize', 'imageContainer');
         this.div.appendChild(this.#imageContainer);
 
+        this.#images = [];
+        for (let i = 0; i < canvasFramesArray.length; i++) {
+            let img = ui.CreateImage(canvasFramesArray[i]);
+            ui.AddClassesToDOM(img, 'canvasSize', 'image', CanvasSize.canvasNumToName(i));
+            if (i == 0) {
+                this.#imgBase = img;
+            } else {
+                ui.AddClassToDOMs('frame', img);
+                this.#images.push(img);
+            }
+            this.#imageContainer.appendChild(img);
+        }
+
+        this._addHelpIcon(`${componentTitle}`);
     }
 
+    /**
+     * 
+     * @param {string} name 
+     */
+    static canvasNameToNum(name) {
+        if (!name) { return null; }
+        switch (name.toLowerCase().replaceAll(' ', '')) {
+            case 'all':
+                return 0;
+            case 'sm':
+            case 'small':
+                return 1;
+            case 'smp':
+            case 'smallplus':
+                return 2;
+            case 'md':
+            case 'med':
+            case 'medium':
+                return 3;
+            case 'mdp':
+            case 'medp':
+            case 'medplus':
+            case 'mediumplus':
+                return 4;
+            case 'lg':
+            case 'large':
+                return 5;
+            case 'lgp':
+            case 'largeplus':
+                return 6;
+        }
+        return null;
+    }
+    static canvasNumToName(num, fullName = false) {
+        switch (num) {
+            case 0:
+                return 'all';
+            case 1:
+                return fullName ? 'small' : 'sm';
+            case 2:
+                return fullName ? 'small plus' : 'smp';
+            case 3:
+                return fullName ? 'medium' : 'md';
+            case 4:
+                return fullName ? 'medium plus' : 'mdp';
+            case 5:
+                return fullName ? 'large' : 'lg';
+            case 6:
+                return fullName ? 'large plus' : 'lgp';
+        }
+    }
 }
