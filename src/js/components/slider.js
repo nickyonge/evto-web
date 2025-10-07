b5846f5fdc10286d3e1ebd481210e53590fb5c18
nickyonge <nick@duckpond.studio>
2025-10-07 15:57:50 -0700

    generate slider (needs work)

--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -1,3 +1,4 @@
+import { GetCSSVariable } from "../lilutils";
 import * as ui from "../ui";
 import { TitledComponent } from "./base";
 
@@ -8,9 +9,28 @@ import { TitledComponent } from "./base";
  */
 export class Slider extends TitledComponent {
 
+    #input;
+    #bg;
+
     constructor(componentTitle) {
         super(componentTitle);
+
+        ui.AddClassesToDOM(this.div, 'slider', 'container');
+
+        this.#input = ui.CreateInputWithID('range', this.uniqueComponentName, 'sinput');
+        ui.AddElementAttributes(this.#input, ['min', 'max', 'step', 'value'], [0, 100, 5, 50]);
+        this.#bg = ui.CreateElementWithClass('span', 'sbg');
+        ui.AddElementAttribute(this.#bg, 'value', 50);
+
+        this.#input.addEventListener('input', (e) => {
+            this.#bg.style.setProperty('--slider-value', `${e.target.value}%`)
+        });
+
+        this.div.appendChild(this.#input);
+        this.div.appendChild(this.#bg);
+
         this._addHelpIcon(componentTitle);
+
     }
 
     /* Example HTML
