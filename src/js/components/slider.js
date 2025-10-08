f550f156e63691286db235a27e80b99c1e518e2a
nickyonge <nick@duckpond.studio>
2025-10-07 18:44:09 -0700

    tickmarks in sliders

--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -11,6 +11,8 @@ export class Slider extends TitledComponent {
 
     #input;
     #bg;
+    #ticksContainer;
+    #ticks;
 
     constructor(componentTitle) {
         super(componentTitle);
@@ -22,6 +24,20 @@ export class Slider extends TitledComponent {
         this.#bg = ui.CreateElementWithClass('span', 'sbg');
         ui.AddElementAttribute(this.#bg, 'value', 50);
 
+        // generate tickmarks
+        this.#ticksContainer = ui.CreateDivWithClass('stickmarks');
+        this.#bg.appendChild(this.#ticksContainer);
+        let min = parseInt(this.#input.min);
+        let max = parseInt(this.#input.max);
+        let step = parseInt(this.#input.step);
+        this.#ticks = [];
+        for (let i = min; i <= max; i += step) {
+            let tick = ui.CreateElement('span');
+            this.#ticks.push(tick);
+            this.#ticksContainer.appendChild(tick);
+        }
+
+        // add callback event 
         this.#input.addEventListener('input', (e) => {
             this.#bg.style.setProperty('--slider-value', `${e.target.value}%`)
         });
