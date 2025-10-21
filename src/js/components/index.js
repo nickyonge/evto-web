710186148c173ee5817bdd9d0846e40d8dec19d4
nickyonge <nick@duckpond.studio>
2025-10-21 14:34:19 -0700

    cleanup

--- src/js/components/index.js
+++ src/js/components/index.js
@@ -2,14 +2,15 @@ export { CanvasSize } from "./canvassize.js";
 export { ColorPicker } from "./colorpicker.js";
 export { DropdownList } from "./dropdown.js";
 export { HelpIcon } from "./helpicon.js";
+export { ImageField } from './imagefield.js';
 export { MutliOptionList } from "./multioption.js";
 export { Slider } from "./slider.js";
-export { ImageField } from './imagefield.js';
 export { TextField } from "./textfield.js";
 export { Toggle } from "./toggle.js";
 
 import { GenerateCSS as SliderCSS } from "./slider.js";
 
+/** Generate dynamic CSS across any components that require CSS at runtime */
 export function GenerateCSS() {
     SliderCSS();
 }
