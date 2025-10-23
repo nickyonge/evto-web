9ae2cc7f5ae6b6ffd2b5a56a6fc3e4d4e798dd79
nickyonge <nick@duckpond.studio>
2025-10-23 16:44:42 -0700

    testing recursive proxy object for asset export

--- src/js/assetExporter.js
+++ src/js/assetExporter.js
@@ -1,3 +1,59 @@
+import { isStringNotBlank, ReturnStringNotBlank } from "./lilutils";
+
+export function testExport() {
+    console.log("testing export");
+    console.log(gcs.equator.dotted);
+
+    console.log("creating object");
+    let obj = recursiveProxyObject("test", "end", '#');
+    console.log("testing:");
+    console.log("PROCESSING: " + obj.cookie.chocolatechip.mint);
+}
+
+const defaultAssetExtension = '.png';
+
+function Asset(assetFolderReference, assetName, assetExtension = defaultAssetExtension) {
+    let assetPath = '';
+    switch (assetFolderReference) {
+        case 'gcs':
+            assetPath = '../../assets/png/map/gcs/gcs-';
+            break;
+    }
+    return `${assetPath}${assetName}${assetExtension}`
+}
+
+const gcs = {
+    complete: Asset('gcs', 'complete'),
+    equator: {
+        dotted: Asset('gcs', 'equator_dotted')
+    }
+}
+
+/**
+ * Take symbol values and recursively convert it to a string, or nested object
+ * @param {string} [prefix=''] Optional prefix before values  
+ * @param {string} [suffix=''] Optional suffix before values 
+ * @param {boolean} [returnAsString=true] Return values converted to string? If not, returns directly as object 
+ * @param {[string|symbol]} __path Internal reference to recursive path generation. Leave blank 
+ * @returns {object|string} New object or string, with recursively-defined values  
+ * @example 
+ * let myObject = recursiveProxyObject()
+ * console.log()
+ */
+function recursiveProxyObject(prefix = '', suffix = '', separator = '.', returnAsString = true, __path = []) {
+    return new Proxy({}, {
+        get(_, property) {
+            // porperty type conversion, check to return final output 
+            if (property === Symbol.toPrimitive || property === 'toString' || property === 'valueOf') {
+                const result = `${ReturnStringNotBlank(prefix,null,separator)}${__path.join(separator)}${ReturnStringNotBlank(suffix,separator)}`;
+                return () => (returnAsString ? String(result) : result);
+            }
+            return recursiveProxyObject(prefix, suffix, separator, returnAsString, [...__path, property]);
+        }
+    });
+}
+
+/*
 
 export {
     'gcs-complete.png',
@@ -123,3 +179,4 @@ export {
     'text_white/titlebox-text_title.png'
 
 } from '../../assets/png/map/titlebox/';
+*/
